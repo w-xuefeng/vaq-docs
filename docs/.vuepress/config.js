6d81eed3a892ddd125a8fd9815a615ea7be0aafd
w-xuefeng <xuefeng@kcalb.wang>
2020-02-08 22:08:38 +0800

    feat: add pwa config and asset

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,17 @@ module.exports = {
       description: 'Vue-APICloud-Quickstart 文档'
     }
   },
+  head: [
+    ['link', { rel: 'icon', href: `/img/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   themeConfig: {
     repo: 'w-xuefeng/vue-apicloud-quickstart',
     docsRepo: 'w-xuefeng/vaq-docs',
